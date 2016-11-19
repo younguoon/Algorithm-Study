4a9680bb7d51163528053f4996c097c17419ae7b
DELL <DELL@DESKTOP-E5R68MI>
2016-11-20 02:31:57 +0900

    `16.11.20 _ 알고리즘 스터디, 주석추가

--- AlgorithmStudy/GroupStudy/backjoon/D1120_N2011_HackTest.java
+++ AlgorithmStudy/GroupStudy/backjoon/D1120_N2011_HackTest.java
@@ -28,49 +28,85 @@ package backjoon;
 import java.io.*;
 
 public class D1120_N2011_HackTest {
-	static int[] cache = new int[5001]; //정수 배열 선언
-	static String str;//글자 변수선언
-	static int len; //글자 길이 변수선언
-	static final int MOD = 1000000; //최대값은 변하면 안되므로 final
-
+	// 정수 숫자 배열 선언
+	static int[] cache = new int[5001];
+	// 글자 변수선언
+	static String str;
+	// 글자 길이 변수선언
+	static int len;
+	// 최대값은 변하면 안되므로 final
+	static final int MOD = 1000000;
+	
 	public static void main(String[] args) throws IOException {
+		// 문자, 배열, 행을 버퍼에 담은 후 문자형 입력스트림으로 텍스트를 읽어 들인다.
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		str = br.readLine(); //입력받은 숫자 저장
-		len = str.length(); //숫자 길이 저장
 
-		for (int i = 0; i < cache.length; i++)
-			cache[i] = -1;
-
-		int result = 0; 
-
+		System.out.println("숫자를 입력하세요");
+		// 입력받은 숫자 저장
+		// read대신 readLine을 사용해서 버퍼초기화를 고려하지 않도록 한다.
+		str = br.readLine();
+		// 숫자 길이 저장
+		len = str.length();
 		
-		if (str.charAt(0) != '0')
-			result = (result % MOD + solution(0) % MOD) % MOD;
-		if ((str.charAt(0) == '2' && str.charAt(1) <= '6') || str.charAt(0) == '1')
-			result = (result % MOD + solution(1) % MOD) % MOD;
+		for (int i = 0; i < cache.length; i++) {
+			cache[i] = -1;
+		}
 
+		// 입력값의 첫번째 숫자가 0이면 무조건 0가지가 가능
+		int result = 0;
+
+		// 입력값의 첫번째 숫자가 0이 아닐 때
+		if (str.charAt(0) != '0') {
+			// solution 메소드에 index값을 0으로 넘겨서 값을 받아옴
+			// result는 100만개가 넘어갔을 때 이야기임
+			result = (solution(0) % MOD) % MOD;
+		}
+
+		// 입력값의 첫 번째 숫자가 2 이고 두 번째 숫자가 6 이하이거나,
+		// 입력값의 첫 번째 숫자가 1일 때
+		if ((str.charAt(0) == '2' && str.charAt(1) <= '6') || str.charAt(0) == '1') {
+			// solution 메소드에 index값을 1로 넘겨서 값을 받아옴
+			result = (solution(1) % MOD) % MOD;
+		}
 		System.out.println(result);
 	}
 
+	// index는 무조건 0 or 1로 받아옴
 	static int solution(int index) {
-		 int result = 0;
+		// result값 초기화
+		int result = 0;
 
-		if (index == len - 1)
+		// index값과 입력된 숫자의 길이가 같으면
+		// 1을 리턴
+		if (index == len - 1) {
 			return 1;
+		}
 
-		if (cache[index] != -1)
-  			return cache[index];
+		// 입력받은 숫자 배열의 index가 -1이 아니면,
+		// 입력받은 숫자 배열의 index를 리턴
+		if (cache[index] != -1) {
+			return cache[index];
+		}
 
-		if (index + 1 < len)
+		// index값+1이 입력된 숫자 길이보다 작으면,
+		if (index + 1 < len) {
+			// 입력받은 숫자의 (index+1)번째의 숫자가 0이 아니면,
 			if (str.charAt(index + 1) != '0')
+				// ??
 				result = (result % MOD + solution(index + 1) % MOD) % MOD;
+		}
 
-		if (index + 2 < len)
-			if ((str.charAt(index + 1) == '2' && str.charAt(index + 2) <= '6') || (str.charAt(index + 1) == '1'))
+		// index값+2이 입력된 숫자 길이보다 작으면,
+		if (index + 2 < len) {
+			// 입력받은 숫자의 (index+1)번째 숫자가 2이고, (index+2)번째 숫자가 6이거나,
+			// 입력받은 숫자의 (index+1)번째 숫자가 1이면,
+			if ((str.charAt(index + 1) == '2' && str.charAt(index + 2) <= '6') || (str.charAt(index + 1) == '1')) {
+				
 				result = (result % MOD + solution(index + 2) % MOD) % MOD;
-
-		return cache[index] = result;
+			}
+		}
+		
+		//숫자 배열을 result에 저장하고 리턴
+		return cache[index]= result;
 	}
 }
-
-
