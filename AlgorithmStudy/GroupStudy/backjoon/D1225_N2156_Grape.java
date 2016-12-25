a70218326b792a9aaac679e57ae4b88d93e486ba
DELL <DELL@DESKTOP-E5R68MI>
2016-12-25 09:33:44 +0900

    a

--- AlgorithmStudy/GroupStudy/backjoon/D1225_N2156_Grape.java
+++ AlgorithmStudy/GroupStudy/backjoon/D1225_N2156_Grape.java
@@ -1,39 +1,43 @@
+//https://www.acmicpc.net/problem/2156
+//가중치의 합의 최대
+
 package backjoon;
 
-import java.io.*;
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class D1225_N2156_Grape {
-    public static void main(String[] args) throws IOException {
-        new D1225_N2156_Grape().run();
-    }
 
-    int N;
-    int[] A;
-    int[][] DP;
-    
-    private void run() throws IOException {
-        Scanner sc = new Scanner(System.in);
-        BufferedReader bw = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bw.readLine());
-        A = new int[N+1];
-        DP = new int[N+1][3];
-        for (int i = 1; i <= N; i++) {
-            A[i] = Integer.parseInt(bw.readLine());
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        int max = 0;
-        for (int ix = 1; ix <= N; ix++) {
-        	for (int cnt = 0; cnt <= 2; cnt++) {
-        		if (cnt == 0) {
-        			DP[ix][cnt] = Math.max(DP[ix-1][2], Math.max(DP[ix-1][0], DP[ix-1][1]));
-        		} else {
-        			DP[ix][cnt] = DP[ix-1][cnt-1] + A[ix];
-        		}
-        		max = Math.max(max, DP[ix][cnt]);
-        	}
-        }
-
-        System.out.println(max);
-    }
+		int n = Integer.parseInt(in.readLine());
+		System.out.println("n : "+n);
+		int[] d = new int[n + 1];
+		int[] wine = new int[n + 1];
+		
+		for(int i = 1 ; i <= n ; i++) {
+			wine[i] = Integer.parseInt(in.readLine()); 
+		}
+		
+		d[1] = wine[1];
+		
+		if(n >= 2)
+			d[2] = wine[1] + wine[2];
+		
+		for(int i = 3 ; i <= n ; i++) {
+			// 한잔도 안 마셨을 때
+			d[i] = d[i - 1]; 
+			System.out.println("d[i]"+d[i]);
+			if(d[i] < d[i - 2] + wine[i])
+				// 한잔도 안 마셨을 때
+				d[i] = d[i - 2] + wine[i];
+			if(d[i] < d[i - 3] + wine[i] + wine[i - 1])
+				// 한잔도 안 마셨을 때
+				d[i] = d[i - 3] + wine[i] + wine[i - 1];
+		}
+		
+		System.out.println(d[n]);
+	}
 }
--- AlgorithmStudy/GroupStudy/backjoon/D1225_N2579_Stairs.java
+++ AlgorithmStudy/GroupStudy/backjoon/D1225_N2579_Stairs.java
@@ -1,30 +1,50 @@
+//https://www.acmicpc.net/problem/2579
+
 package backjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class D1225_N2579_Stairs {
-	static int[] D = new int[10001];
-	static int T, N;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException{
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String temp[] = new String[2];
-		temp = br.readLine().split(" ");
-		N = Integer.parseInt(temp[0]);
-		T = Integer.parseInt(temp[1]);
-
-		D[0] = 1;
-
-		for (int i = 1; i <= N; i++) {
-			int c = Integer.parseInt(br.readLine());
-			for (int j = c; j <= T; j++) {
-				D[j] += D[j - c];
-			}
+		
+		int input = Integer.parseInt(br.readLine().trim());
+		int stairs[] = new int[input+1];
+		for(int i=1 ; i <= input ; i++) {
+			stairs[i] = Integer.parseInt(br.readLine().trim());
+		} // 입력완료
+		
+		int dp[][] = new int[input+1][3];
+		
+		// dp 앞에는 현재 위치. 뒤에는 직전에 움직인 계단 수.
+		// 이전에 1칸으로 올라왔으면 그 다음번에 1칸만 오를 수 없다.
+		dp[1][1] = stairs[1];
+		dp[1][2] = stairs[1]; 
+		
+//		dp[2][1] = stairs[1] + stairs[2];
+//		dp[2][2] = stairs[2];
+//		
+//		dp[3][1] = dp[2][2] + stairs[3];
+//		dp[3][2] = Math.max(dp[1][1] , dp[1][2]) + stairs[3]; //dp[1][2]는 원래는 없는값.? 
+//		
+//		dp[5][1] = dp[4][2] + stairs[5];
+//		dp[5][2] = Math.max(dp[3][1],dp[3][2]) + stairs[5];
+//		
+//		dp[7][1] = dp[7][2] + stairs[7];
+//		dp[7][2] = Math.max(dp[5][1], dp[5][2]) + stairs[7];
+		
+		// dp[3][1] 3번째 계단을 1칸씩 오르는걸로는 갈 수 없다. (문제조건)
+		for(int i=2 ; i <= input ; i++) {
+			dp[i][1] = dp[i-1][2] + stairs[i];
+			dp[i][2] = Math.max(dp[i-2][1], dp[i-2][2]) + stairs[i];
 		}
-
-		System.out.println(D[T]);
-
+		
+		System.out.println(Math.max(dp[input][1], dp[input][2]));
+		
 	}
 
 }
